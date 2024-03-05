1ddc29acbe580fb6188105a71a9b76d42aa300c1
bashair.alolwi@gmail.com <bashair.alolwi@gmail.com>
2024-03-05 14:28:45 +0000

    commit orders_margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,7 +1,7 @@
 select
   orders_id,
   date_date,
-  ROUND(SUM(revenue),2) as revenue ,
+  ROUND(SUM(revenue),2) as revenue,
   ROUND(SUM(quantity),2) as quantity ,
   ROUND(SUM(purchase_cost),2) as purchase_cost,
   ROUND(SUM(margin),2) as margin ,
